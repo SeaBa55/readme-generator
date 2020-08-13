17e713c5163505793eab91b3b023704df37f032f
sarrazol <sebastianarrazola@gmail.com>
2020-08-13 18:09:48 -0400

    Initial working concept - need to add a few questions to my inquirer prompts

--- index.js
+++ index.js
@@ -1,16 +1,83 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
-
+    {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+    },
+    {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+    },
+    {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project",
+    },
+    {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
+    },
+    {
+    type: "input",
+    name: "dependencies",
+    message: "What command should be run to install your dependencies?",
+    },
+    {
+    type: "input",
+    name: "tests",
+    message: "What command should be run to run your tests?",
+    },
+    {
+    type: "input",
+    name: "repo",
+    message: "What does the user need to know about using the repo?",
+    },
+    {
+    type: "input",
+    name: "contributions",
+    message: "What does the user need to know about contributing to the repo?",
+    }
 ];
 
+function userPrompt() {
+    return inquirer.prompt(questions);
+}
+
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName,data) {
+    fs.writeFile(fileName,generateMarkdown(data),function(error) {
+
+        console.log(fileName + " generated");
+
+        if(error) {
+            throw error;
+        }
+    });
 }
 
-// function to initialize program
-function init() {
+userPrompt()
+.then(function(data) {
+    writeToFile("gooseREADME.md",data);
+})
+.then(function(){
+    console.log("success");
+})
+.catch(function(error) {
+    console.log(error);
+});
 
-}
+// // function to initialize program
+// function init() {
 
-// function call to initialize program
-init();
+// }
+
+// // function call to initialize program
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,50 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data)
+  return `
+  
+  # ${data.title}
 
-`;
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#licesnse)
+  * [Test](#tests)
+  * [Questions](#questions)
+
+  ## Instalation
+
+  To install required dependencies, run the following command:
+  > ${data.dependencies}
+
+  ## Usage Description
+
+  ${data.repo}
+
+  ## License
+  
+  ![License](https://img.shields.io/badge/License-${data.license}-blue.svg)
+  https://img.shields.io/github/license/<Github-Username>/<Repository>
+
+  ## Contributing
+
+  ${data.contributions}
+
+  ## Tests
+
+  To run tests, run the following command:
+  > ${data.tests}
+
+  ## Questions
+
+  If you have any questions or issues, please contact me at ${data.email}. You can find more information of my work at ${data.github}.
+
+  `;
 }
 
 module.exports = generateMarkdown;
